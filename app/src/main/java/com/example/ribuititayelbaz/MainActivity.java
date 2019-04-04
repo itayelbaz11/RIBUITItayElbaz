bb172337e6739e67412f342da672616b1a66dab5
itayelbaz11 <aa0801@bs.amalnet.k12.il>
2019-04-04 09:34:43 +0300

    8וטפ7ןפ

---
+++ app/src/main/java/com/example/ribuititayelbaz/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.ribuititayelbaz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btn(View view) {
+    }
+
+    public void btn2(View view) {
+    }
+
+    public void btn3(View view) {
+    }
+}
